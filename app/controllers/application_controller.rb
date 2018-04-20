2b3d9eb830971c9d3207f62429c2d8ecacf4d3aa
Nic Devlin <nic@nicdevlin.com.au>
2018-04-20 12:10:01 +1000

    attempt two

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
-HOSTNAME = ENV['HOSTNAME']
 end
