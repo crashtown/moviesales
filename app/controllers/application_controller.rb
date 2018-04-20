99638a5d12c3f053c2e7f68877539abbddf82447
Nic Devlin <nic@nicdevlin.com.au>
2018-04-20 11:48:34 +1000

    postgres conversion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  Bundler.require(*Rails.groups)
+
+Dotenv::Railtie.load
+
+HOSTNAME = ENV['HOSTNAME']
 end
